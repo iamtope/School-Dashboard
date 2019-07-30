281082d0ad907722b2174cfd6f086c7c6b60ffc3
Abass T. Adamo <abass.adamo@prodeus.com.ng>
2019-07-30 11:43:24 +0100

    first commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="holder">
+  <!-- sidebar Holder -->
+  <nav id="sidebar" class="sidebar">
+    <div class="sidebar-header">
+      <img src="./images/school-logo.svg" alt="school logo"/><span>Handmaid's International</span>
+    </div>
+  </nav>
+</div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Khula&display=swap');
+html, body{
+  font-family: 'Khula', sans-serif;
+}
+</style>
